0a5ec958223c78ba401ae35019f619749126539f
Kristoffer Ahlstrom-Vij <ahlstromvij@Kristoffers-MacBook-Pro.local>
2022-10-12 14:27:33 +0100

    Test for info_effect with and without survey weights

--- tests/testthat/test-inf_effect.R
+++ tests/testthat/test-inf_effect.R
@@ -38,7 +38,7 @@ test_that("Propensity scores are calculated correctly", {
   expect_equal(prop_scores[1], 2.397574, tolerance = 1e-5)
 })
 
-test_that("Information effects are calculated correctly with CIs", {
+test_that("Information effects are calculated correctly with CIs and survey weights", {
   set.seed(1)
   outcome_var <- sample(c(1,0), replace=TRUE, size=100)
   knowledge_binary <- sample(c(1,0), replace=TRUE, size=100)
@@ -64,7 +64,31 @@ test_that("Information effects are calculated correctly with CIs", {
   expect_equal(eff$difference, -0.01861934, tolerance = 1e-5)
 })
 
-test_that("Information effects are calculated correctly without CIs", {
+test_that("Information effects are calculated correctly with CIs and without survey weights", {
+  set.seed(1)
+  outcome_var <- sample(c(1,0), replace=TRUE, size=100)
+  knowledge_binary <- sample(c(1,0), replace=TRUE, size=100)
+  education <- sample(c("none","gcse","alevel","university"), replace=TRUE, size=100)
+  income <- sample(c("Q1","Q2","Q3","Q4"), replace=TRUE, size=100)
+  prop_scores <- abs(rnorm(100,0,1))
+  df <- data.frame(outcome_var, knowledge_binary, education, income, prop_scores)
+  eff <- info_effect(outcome = "outcome_var",
+                     knowledge_var = "knowledge_binary",
+                     covariates = c("education",
+                                    "income"),
+                     prop_weight = "prop_scores",
+                     data = df,
+                     boot_ci = T)
+  expect_equal(eff$actual_proportion, 0.49, tolerance = 1e-5)
+  expect_equal(eff$actual_upr, 0.59, tolerance = 1e-5)
+  expect_equal(eff$actual_lwr, 0.39, tolerance = 1e-5)
+  expect_equal(eff$informed_proportion, 0.5015549, tolerance = 1e-5)
+  expect_equal(eff$informed_upr, 0.5315032, tolerance = 1e-5)
+  expect_equal(eff$informed_lwr, 0.471042, tolerance = 1e-5)
+  expect_equal(eff$difference, 0.01155488, tolerance = 1e-5)
+})
+
+test_that("Information effects are calculated correctly without CIs but with survey weights", {
   set.seed(1)
   outcome_var <- sample(c(1,0), replace=TRUE, size=100)
   knowledge_binary <- sample(c(1,0), replace=TRUE, size=100)
@@ -83,5 +107,25 @@ test_that("Information effects are calculated correctly without CIs", {
   expect_equal(eff$actual_proportion, 0.5089763, tolerance = 1e-5)
   expect_equal(eff$informed_proportion, 0.4903569, tolerance = 1e-5)
   expect_equal(eff$difference, -0.01861934, tolerance = 1e-5)
+})
+
+test_that("Information effects are calculated correctly without CIs and survey weights", {
+  set.seed(1)
+  outcome_var <- sample(c(1,0), replace=TRUE, size=100)
+  knowledge_binary <- sample(c(1,0), replace=TRUE, size=100)
+  education <- sample(c("none","gcse","alevel","university"), replace=TRUE, size=100)
+  income <- sample(c("Q1","Q2","Q3","Q4"), replace=TRUE, size=100)
+  prop_scores <- abs(rnorm(100,0,1))
+  survey_wt <- abs(rnorm(100,0,1))
+  df <- data.frame(outcome_var, knowledge_binary, education, income, prop_scores)
+  eff <- info_effect(outcome = "outcome_var",
+                     knowledge_var = "knowledge_binary",
+                     covariates = c("education",
+                                    "income"),
+                     prop_weight = "prop_scores",
+                     data = df)
+  expect_equal(eff$actual_proportion, 0.49, tolerance = 1e-5)
+  expect_equal(eff$informed_proportion, 0.5015549, tolerance = 1e-5)
+  expect_equal(eff$difference, 0.01155488, tolerance = 1e-5)
 })
 
